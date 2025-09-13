c5fb8298e01449c202997af8a1f6e2547920ad6e
DuanrA <arnaud.scrivo@gmail.com>
2025-09-14 00:50:39 +0200

    ajout de la navbar

--- portfolioreactduanra/src/app/page.js
+++ portfolioreactduanra/src/app/page.js
@@ -1,12 +1,12 @@
 "use client";
-
-import Image from "next/image";
 import DarkVeil from "@/components/DarkVeil";
+import Navbar from "@/components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <div className="fixed inset-0 w-full h-full">
       <DarkVeil />
+      <Navbar />
     </div>
   );
 }
