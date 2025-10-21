6a148df5c4d1bafbb3653d52ec492ba192c0695b
screwedupclick <arnaudscrivo.dev@proton.me>
2025-10-21 13:16:37 +0200

    ajout des informations dans le componsant Infos ainsi que du CSS associé

--- portfolioreactduanra/src/components/Infos/index.jsx
+++ portfolioreactduanra/src/components/Infos/index.jsx
@@ -3,41 +3,18 @@ import "./Infos.css";
 import { useSearchParams } from "next/navigation";
 import { useEffect } from "react";
 
-const Infos = () => {
-  const [info, setInfo] = useState({
-    name: "Arnaud",
-    job: "Web Developer",
-    email: "arnaudscrivo.dev@proton.me",
-  });
-
-  const searchParams = useSearchParams();
-
-  useEffect(() => {
-    const name = searchParams.get("name");
-    const job = searchParams.get("job");
-    const email = searchParams.get("email");
-
-    if (name || job || email) {
-      setInfo({
-        name: name || info.name,
-        job: job || info.job,
-        email: email || info.email,
-      });
-    }
-  }, [searchParams, info.name, info.job, info.email]);
-
+function Infos() {
   return (
     <div className="infos">
-      <div className="infos-container">
-        <div className="infos-content">
-          <h1 className="infos-title">
-            {info.name} {info.job}
-          </h1>
-          <p className="infos-subtitle">{info.email}</p>
-        </div>
-      </div>
+      <p className="infos__text">
+        Bonjour ! Je m'appelle Arnaud, et je suis développeur web. J'ai terminé
+        ma reconversion professionnelle en Octobre 2025. Passionné
+        d'informatique, je crée des scripts pour faire du pentesting avec le
+        Flipper Zero. Ma prochaine étape sera de me former en réseau pour
+        devenir un DevOps.
+      </p>
     </div>
   );
-};
+}
 
 export default Infos;
