e69f0d23afca39184be0ac6f022445fb86cb0624
screwedupclick <arnaudscrivo.dev@proton.me>
2025-10-22 16:14:29 +0200

    Import du nouveau composant

--- portfolioreactduanra/src/app/page.js
+++ portfolioreactduanra/src/app/page.js
@@ -1,14 +1,14 @@
 "use client";
 import DarkVeil from "@/components/DarkVeil";
 import Navbar from "@/components/Navbar/Navbar";
-import Infos from "@/components/Infos";
+import Pfp from "@/components/PFP";
 
 export default function Home() {
   return (
     <div className="fixed inset-0 w-full h-full">
       <DarkVeil />
       <Navbar />
-      <Infos />
+      <Pfp />
     </div>
   );
 }
